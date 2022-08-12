c8e181bf12a963c9a40ba193ee4ce4b75bd0f98f
Dhanu <110900586+masterdhanu@users.noreply.github.com>
2022-08-12 16:03:53 +0530

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
 @echo off
-TITLE KaizuryuBot
-:: Enables virtual env mode and then starts Kaizuryu
-env\scripts\activate.bat && py -m KaizuryuBot
+TITLE Shinchan Robot
+:: Enables virtual env mode and then starts Shinchan_Robot
+env\scripts\activate.bat && py -m Shinchan-Robot
